2e74f551563172f1a718c43a3475e0503682ad02
Daniil Verkhutin <danverh29@gmail.com>
2022-01-16 18:47:07 +0200

    db attempt

--- templates/db.sql
+++ templates/db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE gogs CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;;
-GRANT ALL ON gogs.* to 'gogs'@'{{ db-ip }}.%.%' IDENTIFIED BY '{{ db_password }}' WITH GRANT OPTION;
+GRANT ALL ON gogs.* to 'gogs'@'{{ ip_db }}.%.%' IDENTIFIED BY '{{ db_password }}' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
